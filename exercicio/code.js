f5312439430c9ee3a3df40f46dfa86f301adab11
Daniel Da Silva <100931944+projdanielsilva@users.noreply.github.com>
2023-03-07 12:38:11 -0300

    07/03

---
+++ exercicio/code.js
@@ -0,0 +1,21 @@
+function troca1(){
+    window.location.href = "index.html"
+
+}
+
+function troca2(){
+
+    window.location.href = "sobre.html"
+}
+
+function troca3(){
+    window.location.href = "tec.html"
+}
+
+function troca4(){
+    window.location.href = "curio.html"
+}
+
+function troca5(){
+    window.location.href = "contato.html"
+}
